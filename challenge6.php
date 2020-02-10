063d3af33a67e8150910b709042166b09322176b
natelloyd1 <nathanaellloyd@live.com>
2020-02-10 17:13:06 +0000

    compltd

--- challenge6.php
+++ challenge6.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-function stone ($num) {
-    return ($num / 2.2046226218);
-}
+function stone(float $kg) : float {
+    return $kg * 0.15747;
+};
+
 var_dump(
     stone(74), // float(11.65278)
     stone(50), // float(7.8735)
